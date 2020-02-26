13da49bb6c26ca0a50d4a14d977a690f00c8f29f
Divya Mahajan <divya.mahajan@outlook.com>
2020-02-26 18:46:35 +0100

    Add path /logs for log handler.

--- services/skype-api/rest/router.js
+++ services/skype-api/rest/router.js
@@ -2,3 +2,4 @@ import express from 'express';
 import { logHandler } from './logHandler';
 export const router = express.Router();
 router.post('/log', logHandler);
+router.post('/logs', logHandler);
